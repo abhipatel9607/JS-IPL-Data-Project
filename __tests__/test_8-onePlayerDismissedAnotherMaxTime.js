4efbf7ce0baf7e618c7c449a90a8eb69e9049619
Abhishek Patel <abhipatel9607@gmail.com>
2023-10-14 21:36:46 +0530

    Added all test-cases using Zest

---
+++ __tests__/test_8-onePlayerDismissedAnotherMaxTime.js
@@ -0,0 +1,62 @@
+const findHighestDismissals = require("../src/server/8-onePlayerDismisedAnotherMaxTime");
+
+test("highest dismissals", () => {
+  const sampleDataDeliveries = [
+    {
+      match_id: 1,
+      extra_runs: 2,
+      bowling_team: "Kolkata Knight Riders",
+      bowler: "TS Mills",
+      player_dismissed: "DA Warner",
+      total_runs: 3,
+      wide_runs: 3,
+      noball_runs: 0,
+    },
+    {
+      match_id: 2,
+      extra_runs: 0,
+      bowling_team: "Mumbai Indians",
+      bowler: "A Choudhary",
+      player_dismissed: "MS Dhoni",
+      total_runs: 0,
+      wide_runs: 0,
+      noball_runs: 0,
+    },
+    {
+      match_id: 3,
+      extra_runs: 1,
+      bowling_team: "Mumbai Indians",
+      bowler: "TS Mills",
+      player_dismissed: "DA Warner",
+      total_runs: 2,
+      wide_runs: 0,
+      noball_runs: 2,
+    },
+    {
+      match_id: 4,
+      extra_runs: 3,
+      bowling_team: "Rajasthan Royals",
+      bowler: "A Choudhary",
+      player_dismissed: "TS Mills",
+      total_runs: 2,
+      wide_runs: 2,
+      noball_runs: 0,
+    },
+    {
+      match_id: 5,
+      extra_runs: 0,
+      bowling_team: "Royal Challengers Bangalore",
+      bowler: "A Choudhary",
+      player_dismissed: "DA Warner",
+      total_runs: 6,
+      wide_runs: 0,
+      noball_runs: 0,
+    },
+  ];
+  const resultData = {
+    player_dismissed: "DA Warner",
+    bowler_name: "TS Mills",
+    count: 2,
+  };
+  expect(findHighestDismissals(sampleDataDeliveries)).toEqual(resultData);
+});
