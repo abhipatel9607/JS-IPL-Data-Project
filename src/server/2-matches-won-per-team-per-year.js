84726ef537ace0210ef99a077b7f99225198f79d
Abhishek Patel <abhipatel9607@gmail.com>
2023-10-21 18:28:14 +0530

    Bug fixed in Problem-2

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -2,13 +2,15 @@ function calcMatchesWonPerTeamPerYear(matches) {
 
   const matchesWonPerTeamPerYear = matches.reduce((acc, match) => {
 
-    if (acc[match.season] && acc[match.season][match.winner]) {
-      acc[match.season][match.winner]++
-    } else if (acc[match.season] && !(acc[match.season][match.winner])) {
-      acc[match.season][match.winner] = 1
-    } else {
-      acc[match.season] = {}
-      acc[match.season][match.winner] = 1
+    if (match.winner) {
+      if (acc[match.season] && acc[match.season][match.winner]) {
+        acc[match.season][match.winner]++
+      } else if (acc[match.season] && !(acc[match.season][match.winner])) {
+        acc[match.season][match.winner] = 1
+      } else {
+        acc[match.season] = {}
+        acc[match.season][match.winner] = 1
+      }
     }
 
     return acc
